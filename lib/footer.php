5cfb05cdaaf9e7930a8400f14b45e5d8eeb2b16d
Ariful Islam <arifsofg@gmail.com>
2018-07-04 16:39:17 +0600

    Footer changes

--- lib/footer.php
+++ lib/footer.php
@@ -9,7 +9,7 @@
   <div class="control-sidebar-bg"></div>
 </div>
 <!-- ./wrapper -->
-<?php include "lib/js/scripts.php"; ?>
+<?php include "lib/js/scripts.php"; //scripts file put at lib/js/scripts/js  ?>
 <?php include("lib/footer_modal.php"); //all modal content and design in footer_modal.php ?>
 
 </body>
