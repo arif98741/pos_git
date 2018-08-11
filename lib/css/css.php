4c5489976729c52e744e3ce4173df7d5d8498d42
Ariful Islam <arif98741@gmail.com>
2018-08-12 05:49:59 +0600

    BASE_URL Added in Sources

--- lib/css/css.php
+++ lib/css/css.php
@@ -1,19 +1,19 @@
 <!-- favicon -->
-<link rel="shortcut icon" href="uploads/favicon.PNG">
+<link rel="shortcut icon" href="<?php echo BASE_URL; ?>uploads/favicon.PNG">
 <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/bower_components/Ionicons/css/ionicons.min.css">
   <!-- DataTables -->
-  <link rel="stylesheet" href="assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="assets/dist/css/skins/_all-skins.min.css">
- <link rel="stylesheet" href="assets/bower_components/select2/dist/css/select2.min.css">
+  <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/dist/css/skins/_all-skins.min.css">
+ <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/bower_components/select2/dist/css/select2.min.css">
 <!--   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css"> 
    -->
 
@@ -25,5 +25,5 @@
   <![endif]-->
 
   <!-- Google Font -->
-  <!-- <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"> -->
+ <link rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"> 
--- lib/js/scripts.php
+++ lib/js/scripts.php
@@ -1,25 +1,25 @@
 
 <!-- jQuery 3 -->
-<script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- DataTables -->
-<script src="assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <!-- SlimScroll -->
-<script src="assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<script src="assets/bower_components/select2/dist/js/select2.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/bower_components/select2/dist/js/select2.min.js"></script>
 
 <!-- Latest compiled and minified JavaScript -->
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script> -->
 
 <!-- AdminLTE App -->
-<script src="assets/dist/js/adminlte.min.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="assets/dist/js/demo.js"></script>
-<script src="assets/dist/js/custom.js"></script>
-<script src="assets/dist/js/transactionandsuppliertransaction.js"></script>
-<script src="assets/dist/js/select2.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/dist/js/demo.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/dist/js/custom.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/dist/js/transactionandsuppliertransaction.js"></script>
+<script src="<?php echo BASE_URL; ?>assets/dist/js/select2.js"></script>
 
 <!-- page script -->
 <script>
