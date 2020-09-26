c887bbc5e20fc4d4607fb191d0c6c0bc71cfddcd
Ariful Islam <arif98741@gmail.com>
2020-09-26 15:16:37 +0600

    data credentials changed for deploy

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'rizan_inventory');
+define('USERNAME', 'wjnomemz_sazzad');
+define('PASSWORD', 'Sazzad@3424590&^');
+define('DATABASE', 'wjnomemz_inventory');
 
 date_default_timezone_set('Asia/Dhaka');
