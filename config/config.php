ed32df06584ef347da7685ed2684db41037b195f
Ariful Islam <arif98741@gmail.com>
2020-09-26 16:06:26 +0600

    changes footer credit

--- config/config.php
+++ config/config.php
@@ -3,5 +3,6 @@ define('HOST', 'localhost');
 define('USERNAME', 'root');
 define('PASSWORD', '');
 define('DATABASE', 'rizan_inventory');
+define('PROJECT', 'Glamy Girl');
 
 date_default_timezone_set('Asia/Dhaka');
